18f7820cd09f0a17bdec0babbe5cfd9112092827
jk-ramos <jaquelaineramos@hotmail.com.br>
2025-11-03 02:47:33 -0300

    atv1

---
+++ src/main/java/br/com/fatec/atv1/controller/NomeController.java
@@ -0,0 +1,17 @@
+package br.com.fatec.atv1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+
+public class NomeController {
+
+    @GetMapping("/nome")
+    public String retornaNome(){
+        return "Jaquelaine Ramos";
+    }
+
+}
